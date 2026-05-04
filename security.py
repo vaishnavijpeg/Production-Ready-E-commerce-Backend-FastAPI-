a13b9865d6356515283d97b2b10947c4ee724041
Vaishnavi <123186238+vaishnavijpeg@users.noreply.github.com>
2026-05-04 16:09:08 +0530

    Implement JWT access token creation function

---
+++ security.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+SECRET_KEY = "SECRET"
+ALGORITHM = "HS256"
+
+def create_access_token(data: dict):
+    to_encode = data.copy()
+    expire = datetime.utcnow() + timedelta(hours=24)
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
